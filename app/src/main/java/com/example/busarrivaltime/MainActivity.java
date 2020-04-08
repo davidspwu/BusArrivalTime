0da81f02a51b9a84a4fe52070d99577ea3988cce
Your Name <you@example.com>
2020-04-07 21:41:52 -0700

    Add option menu.

--- app/src/main/java/com/example/busarrivaltime/MainActivity.java
+++ app/src/main/java/com/example/busarrivaltime/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.busarrivaltime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.busarrivaltime.ui.main.MainFragment;
 import com.example.busarrivaltime.ui.main.RouteFragment;
+import com.example.busarrivaltime.ui.main.dummy.DummyContent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RouteFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,32 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, RouteFragment.newInstance(1))
                     .commitNow();
         }
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
